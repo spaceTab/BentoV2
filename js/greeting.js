75b7c8ec794f79547338086b79fb6c01833e96df
spaceTab <93plaitj@protonmail.ch>
2020-11-30 17:05:44 -0500

    changes

---
+++ js/greeting.js
@@ -0,0 +1,27 @@
+// Get the hour
+var today = new Date();
+var hour = today.getHours();
+
+// Here you can change your name
+var name = 'Jake'
+
+// Here you can change your greetings
+var gree1 = 'Go to Sleep! ';
+var gree2 = 'Good morning! ';
+var gree3 = 'Good afternoon ';
+var gree4 = 'Good evening ';
+var gree5 = 'Good evening ';
+var gree6 = 'Good evening ';
+
+// Define the hours of the greetings
+if (hour >= 23 && hour < 5) {
+    document.getElementById('greetings').innerText = gree1 + name;
+} else if (hour >= 6 && hour < 12) {
+    document.getElementById('greetings').innerText = gree2 + name;
+} else if (hour >= 12 && hour < 17) {
+    document.getElementById('greetings').innerText = gree3 + name;
+} else  {
+    document.getElementById('greetings').innerText = gree4 + name;
+}
+
+  
