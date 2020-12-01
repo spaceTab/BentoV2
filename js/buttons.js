36a50c67244210e311a0ae5ef99ab85851bec439
spaceTab <93plaitj@protonmail.ch>
2020-12-01 14:47:43 -0500

    clean up buttons.js

--- js/buttons.js
+++ js/buttons.js
@@ -1,24 +1,17 @@
 console.log('in buttons.js')
 
 window.onkeypress = function(event){
-  // if (event.keyCode == 49) {
-    // console.log('button has been pressed');
-    // document.getElementById('gh-btn').click();
-  // } else if (event.keyCode == 50){
-    // document.getElementById('yt-btn').click();
-  // } else if (event.keyCode == 51) {
-    // document.getElementById('gm-btn').click()
-  // }
 
   switch (event.which) {
     case 49:
       document.getElementById('gh-btn').click();
       break;
-      case 50:
+    case 50:
         document.getElementById('yt-btn').click();
         break;
     case 51:
       document.getElementById('gm-btn').click();
       break;
   }
+
 }
