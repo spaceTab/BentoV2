cb3d9fd2ad013618012e875c94332f36f2eed01a
spaceTab <93plaitj@protonmail.ch>
2020-12-14 17:21:44 -0500

    comment change. . .

--- js/theme.js
+++ js/theme.js
@@ -33,10 +33,10 @@ themeToggle.addEventListener('click', () => {
 
 // Change the theme with the time
 
-// const today = new Date();
-// const Hr = today.getHours();
+/* const today = new Date();
+ const Hr = today.getHours();
 
-/*
+
 if (Hr >= 19 || Hr < 5) {
     enableDark();
 } else {
